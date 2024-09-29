cf1043ecdf79d67ef616c0cf249dc06d244486a1
adrianmacarenco <adrian.macarenco@gmail.com>
2024-09-29 09:32:17 +0200

    feat: xcframework added

--- Package.swift
+++ Package.swift
@@ -4,18 +4,28 @@
 import PackageDescription
 
 let package = Package(
-    name: "WindowsAzureMessaging",
+    name: "WindowsAzureMessagingNotifications",
+    platforms: [.iOS(.v15)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "WindowsAzureMessaging",
-            targets: ["WindowsAzureMessaging"]),
+            name: "WindowsAzureMessagingNotifications",
+            targets: ["WindowsAzureMessagingNotifications"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
+        .binaryTarget(
+            name: "WindowsAzureMessaging",
+            path: "Sources/WindowsAzureMessagingNotifications/WindowsAzureMessaging_rebuilt.xcframework"
+        ),
         .target(
-            name: "WindowsAzureMessaging"),
+            name: "WindowsAzureMessagingNotifications",
+            dependencies: [
+                "WindowsAzureMessaging"
+            ],
+            path: "Sources"
+        ),
 
     ]
 )
